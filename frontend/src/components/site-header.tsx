12b08a0163b8ffc1a72de47087b3f849e8053a87
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2025-06-29 00:57:06 +0530

    feat(site-header): replace button with mode toggle in site header

--- frontend/src/components/site-header.tsx
+++ frontend/src/components/site-header.tsx
@@ -1,22 +1,13 @@
-import { Button } from "@/components/ui/button";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { ModeToggle } from "./mode-toggle";
 
 export function SiteHeader() {
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
-
         <div className="ml-auto flex items-center gap-2">
-          <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
-            <a
-              href="#"
-              rel="noopener noreferrer"
-              className="dark:text-foreground"
-            >
-              Guide
-            </a>
-          </Button>
+          <ModeToggle />
         </div>
       </div>
     </header>
